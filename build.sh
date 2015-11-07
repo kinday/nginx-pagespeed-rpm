0af5dc384756e242a5f24422cf587bd19a637e0e
Leonard Kinday <leonard@kinday.ru>
2015-11-07 21:25:38 +0300

    Update SELinux boolean

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ NPS_VERSION=`grep "define nps_version" SPECS/nginx.spec |cut -d " " -f 3`
 wget https://github.com/pagespeed/ngx_pagespeed/archive/release-${NPS_VERSION}-beta.zip -P SOURCES/
 wget https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}.tar.gz -P SOURCES/
 rpmbuild -ba SPECS/nginx.spec
+setsebool -P httpd_execmem on
